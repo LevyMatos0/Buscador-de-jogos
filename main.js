8478d65c7f6e910452ca01ea324020f79e442899
Levy Matos <levysantosmatos@gmail.com>
2026-03-01 01:56:04 -0300

    Filtro de ordem de notas

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const botao = document.getElementById("buscar")
 
 const filtroNota = document.getElementById("rating")
 const filtroAno = document.getElementById("year")
-
+const filtroOrdem = document.getElementById("order")
 
 
 
@@ -22,89 +22,97 @@ async function buscarJogos() {
     const objeto = await response.json();
     console.log(objeto)
 
-    objeto.results.forEach(game => {
-      const card = document.createElement("div");
-      card.classList.add("card");
+    if (filtroOrdem.value === "rating") {//ordenar por notas se filtro estiver ativo
+      objeto.results.sort((a, b) =>  b.rating - a.rating )}
 
-      if (filtroNota.value === "") {
+      results.innerHTML = "";
 
-      } else if (filtroNota.value === "4") {
-        if (game.rating < 4) {
-          return;
+      objeto.results.forEach(game => {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
+        if (filtroNota.value === "") {
+
+        } else if (filtroNota.value === "4") {
+          if (game.rating < 4) {
+            return;
+
+          }
+        } else if (filtroNota.value === "3") {
+          if (game.rating < 3) {
+            return;
+          }
+        } else {
+          if (game.rating < 2) {
+            return;
+          }
         }
-      } else if (filtroNota.value === "3") {
-        if (game.rating < 3) {
-          return;
+
+        if (filtroAno.value === "") {
+
+        } else if (filtroAno.value === "2024") {
+
+          if (Number(game.released.slice(0, 4)) < 2024) {
+            return;
+
+          }
+        } else if (filtroAno.value === "2023") {
+
+          if (Number(game.released.slice(0, 4)) < 2023) {
+            return;
+          }
         }
-      } else {
-        if (game.rating < 2) {
-          return;
+        else if (filtroAno.value === "2022") {
+
+          if (Number(game.released.slice(0, 4)) < 2022) {
+            return;
+          }
+        } else if (filtroAno.value === "2020") {
+
+          if (Number(game.released.slice(0, 4)) < 2020)
+            return;
+        } else {
+
+          if (Number(game.released.slice(0, 4)) < 2015)
+            return;
         }
-      }
-
-      if (filtroAno.value === "") {
-
-      } else if (filtroAno.value === "2024") {
-
-        if (Number(game.released.slice(0, 4)) < 2024) {
-          return;
-
-        }
-      } else if (filtroAno.value === "2023") {
-
-        if (Number(game.released.slice(0, 4)) < 2023) {
-          return;
-        }
-      }
-      else if (filtroAno.value === "2022") {
-
-        if (Number(game.released.slice(0, 4)) < 2022) {
-          return;
-        }
-      }else if (filtroAno.value === "2020") {
-
-        if (Number(game.released.slice(0, 4)) < 2020)
-          return;
-      } else {
-
-        if (Number(game.released.slice(0, 4)) < 2015)
-          return;
-      }
 
 
-      card.innerHTML = `
+
+
+
+        card.innerHTML = `
           <img src="${game.background_image}">
           <h3>${game.name}</h3>
           <p>⭐ <strong>${game.rating}</strong></p>
           <p>📅 ${game.released?.slice(0, 4)}</p>
         `
 
-      results.appendChild(card);
+        results.appendChild(card);
 
-    })
+      })
 
+
+
+
+    }
 
   }
+  botao.addEventListener("click", buscarJogos);
 
-}
-botao.addEventListener("click", buscarJogos);
+  // ⌨️ Enter no input
+  nome.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      buscarJogos();
+    }
+  });
 
-// ⌨️ Enter no input
-nome.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    buscarJogos();
+
+
+  nome.addEventListener("input", verificarTexto);
+
+  function verificarTexto() {
+    if (nome.value === "") {
+      results.innerHTML = "";
+    }
   }
-});
-
-
-
-nome.addEventListener("input", verificarTexto);
-
-function verificarTexto() {
-  if (nome.value === "") {
-    results.innerHTML = "";
-  }
-}
-
-
